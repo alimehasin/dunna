d05dd7db7439771c52ae404bd41d0290d2aa0044
Hussain7Abbas <hussain.abduljaleel1905@coeng.uobaghdad.edu.iq>
2025-01-28 11:31:22 +0300

    fix(basic.float): remove unnesessary code and wrong error check

--- src/modules/basic/float.ts
+++ src/modules/basic/float.ts
@@ -4,22 +4,12 @@ interface FloatConfig {
   fixed?: number;
 }
 
-function limitFractions(num: number, fixed: number): number {
-  if (num % 1 === 0) {
-    throw new Error("Receive a number without fractions");
-  }
-
-  const [whole, _faractions] = num.toString().split(".");
-
-  const fractions = _faractions.substring(0, fixed);
-
-  return Number.parseFloat(`${whole}.${fractions}`);
-}
-
-export function float({ min = 0, max = 10, fixed = 3 }: FloatConfig = {}): number {
-  const difference = max - min;
-
-  const num = Math.random() * difference + min;
-
-  return limitFractions(num, fixed);
+export function float({
+  min = 0,
+  max = 10,
+  fixed = 3,
+}: FloatConfig = {}): number {
+  // random float = random number * difference + min
+  const num = Math.random() * (max - min) + min;
+  return Number(num.toFixed(fixed));
 }
